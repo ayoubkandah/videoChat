e87c0abb033d5c3d73339be713beb4c849003f1d
ayoubkandah <a.kandah994@gmail.com>
2021-04-22 14:24:33 +0300

    dd

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
 port: '443',
 secure: true,
-proxied: true
+
 })
 const myVideo = document.getElementById('video1')
 myVideo.muted = true
