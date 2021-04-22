666de9d8828664a3acad3bababbac47a61ccd26d
ayoubkandah <a.kandah994@gmail.com>
2021-04-22 20:38:44 +0300

    d

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ io.on('connection', socket => {
   })
   socket.on("startV",(roomId,userId)=>{
     console.log(userId)
-    socke.emit('user-connected', userId)
+    socke.nsp.in(room).emit('user-connected', userId)
 
   })
 })
