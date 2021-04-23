03be669422c1740ea92b5329ad9b92cbe309547d
ayoubkandah <a.kandah994@gmail.com>
2021-04-23 18:20:05 +0300

    e

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ const io = require('socket.io')(server)
 app.use(require("cors")())
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-let client=0;
- let rooms=[]
- let index=0;
- let room=0;
- let userID;
+let client = 0;
+let rooms = []
+let index = 0;
+let room = 0;
+let userID;
 // app.get('/pvp', (req, res) => {
 //   res.redirect(`/pvp`)
 // })
-app.get("/playerDisc/:id",(req,res)=>{
-//--make update and increase value for player id params
-res.render("playerDisc.ejs")
+app.get("/playerDisc/:id", (req, res) => {
+  //--make update and increase value for player id params
+  res.render("playerDisc.ejs")
 })
 
 app.get('/pvp/', (req, res) => {
   res.render('pvp.ejs', { roomId: "req.params.room" })
 })
-app.get("/s/:e",(req,res)=>{
+app.get("/s/:e", (req, res) => {
   console.log(req.params.e)
 
 
@@ -32,67 +32,75 @@ app.get("/s/:e",(req,res)=>{
 io.on('connection', socket => {
 
 
-  socket.on('join-room', ( roomId,userId) => {
-   client++
-   
-    if(client%2!==0){
-      index=client-index-1
-      rooms.push(index) 
-      
+  socket.on('join-room', (roomId, userId) => {
+    client++
+
+    if (client % 2 !== 0) {
+      index = client - index - 1
+      rooms.push(index)
+
       // console.log(rooms[index])
-      room=rooms[index]
+      room = rooms[index]
       // console.log(room,"roooooom")
 
-      socket.Room=room
+      socket.Room = room
       socket.join(room)
-      console.log(room,"player")
-     
+      // console.log(room,"player")
+
 
       // socket.to(room).emit('user-connected', userId)
 
       // console.log(socket.id)
       // socket.emit("waiting")
-   }else if(client%2==0){
-      console.log("starting");
-      console.log(rooms[index])
-      socket.Room=room
+    } else if (client % 2 == 0) {
+      // console.log("starting");
+      // console.log(rooms[index])
+      socket.Room = room
 
       socket.join(room)
-      console.log(room,"player 2")
+      // console.log(room,"player 2")
 
-   index++
-   console.log(userId)
-  //  socket.nsp.to(room).emit("start",room)
-  socket.emit('player2', 2)
-  socket.to(room).emit('user-connected', userId)
-  
+      index++
+      console.log(userId)
+      //  socket.nsp.to(room).emit("start",room)
+      socket.emit('player2', 2)
+      socket.to(room).emit('user-connected', userId, room)
 
-   }else{
-   }
-   //---
+    } else {
+    }
+    //---
     // socket.join(roomId)
 
 
     socket.on('disconnect', () => {
-      if(client>0){
+      if (client > 0) {
         client--;
-        }
-     
-        if(client%2==0&&index>0){
-           index--
-           rooms.pop()
-     
-        }
+      }
+
+      if (client % 2 == 0 && index > 0) {
+        index--
+        rooms.pop()
+
+      }
       // console.log("disc")
-      
+
       socket.to(room).emit('user-disconnected', userId)
     })
   })
-  socket.on("startV",(roomId,userId)=>{
+  socket.on("startV", (roomId, userId) => {
     console.log(userId)
     socket.emit('user-connected', userId)
-
   })
+  socket.on("startG", (roomG) => {
+    console.log("Start Game")
+    socket.nsp.to(roomG).emit("startGaming",roomG)
+  })
+
+  socket.on("p2Turn",roomG=>{
+    socket.to(roomG).emit("yourTurn")
+    
+    })
 })
 
-server.listen(port,()=>{console.log(port,"listtt");})
+
+server.listen(port, () => { console.log(port, "listtt"); })
--- public/script.js
+++ public/script.js
@@ -1,32 +1,36 @@
 
 'use strict'
 
-const socket = io('https://videochat-ak.herokuapp.com/')
-// const options = {
-//   transports: ['websocket'],
-// };
-// const socket = io('localhost:3000/', options); // emmit connection event to server
-let video2=document.getElementById("video2")
-let video1=document.getElementById("video1")
+// const socket = io('https://videochat-ak.herokuapp.com/')
+const options = {
+  transports: ['websocket'],
+};
+const socket = io('localhost:3000/', options); // emmit connection event to server
+let video2 = document.getElementById("video2")
+let video1 = document.getElementById("video1")
 const videoGrid = document.getElementById('video-grid')
 
 const myPeer = new Peer()
 const myVideo = document.getElementById('video1')
 myVideo.muted = true
 const peers = {}
-let player=1
+let player = 1
 let userID;
+let roomP;
+let yourPoints=0;
+let oppPoints=0;
+$("#start").hide()
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
   addVideoStream(myVideo, stream)
-//---
-faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
-//--
+  //---
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  //--
   myPeer.on('call', call => {
     call.answer(stream)
     const video = document.getElementById('video2')
@@ -36,91 +40,118 @@ faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
   })
   const video = document.getElementById('video1')
   console.log(video)
-//---------------------------------------------------------
-video.addEventListener('play', () => {
-  const canvas = faceapi.createCanvasFromMedia(video)
-  // document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  // faceapi.matchDimensions(canvas, displaySize)
-  setInterval(async () => {
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-    // const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    // faceapi.draw.drawDetections(canvas, resizedDetections)
-    // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-  // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  //---------------------------------------------------------
+  video.addEventListener('play', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    const displaySize = { width: video.width, height: video.height }
+    setInterval(async () => {
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+      if (detections.length > 0) {
+        if (detections[0].expressions.happy > 0.75) {
+        }
+      } else if (detections.length <= 0) {
+      }
+    }, 100)
+  })
+  //-----------------------------------------------------------------------
+  socket.on("player2", val => {
+    player = val
+    console.log(player);
+
+  })
+  socket.on('user-connected', (userId, room) => {
+    console.log(video2.srcObject)
+    // console.log(video1.srcObject)
   
- // faceapi.faceExpressionLabels
- // console.log(detections[0].expressions.happy>0.03);
- // console.log(detections);
- // console.log(canvas);
- //  console.log(resizedDetections)
- //  console.log(faceapi.nets)
- //  console.log(faceapi,"//////////")
- //  console.log(faceapi.draw.drawFaceExpressions)
-if(detections.length>0){
-   if(detections[0].expressions.happy>0.75){
-    //  console.log(detections[0].expressions)
-    let h3 = document.createElement('h1')
-h3.textContent="loseeeeeeeeeeeeeee"
-    videoGrid.append(h3)
-    // console.log("Happy");
-   }
-  }else if(detections.length<=0){
-    // console.log("look to the camera");
+    connectToNewUser(userId, stream)
+
+    roomP = room
+    console.log("---")
+    setTimeout(function aa() {
+      console.log("done 6sec")
+      if (video2.srcObject == null) {
+        // connectToNewUser(userId, stream)
+        socket.emit("startV", ROOM_ID, userId)
+      } 
+
+        socket.emit("startG", roomP)
+      
+    }, 8000);
+  })
+  if(video2.srcObject !== null){
+    socket.emit("startG", roomP)
+
   }
-  // console.log(detections)
-  //  if(faceapi.FACE_EXPRESSION_LABELS=="happy"){
-    //    console.log("happy")
-    //  }
-  }, 100)
 })
-//-----------------------------------------------------------------------
-socket.on("player2",val=>{
-  player=val
-  console.log(player);
+socket.on("startGaming",roomG=>{
+roomP=roomG
+$("#p2").text("Player 2")
+$("#start").show()
+if(player==1){
+  GameStart()
+}else if(player==2){
+  $("#turn").text("Opponent turn")
+$("hint").text("Dont laughing")
+}
 
-  // console.log(userID);
-  // socket.emit("startV",ROOM_ID,userID)
+
 })
-socket.on('user-connected', userId => {
-  console.log(video2.srcObject)
-// console.log(video1.srcObject)
-// connectToNewUser(userId, stream)
-console.log(player);
- connectToNewUser(userId, stream)
-// socket.emit("startV",ROOM_ID,userId)
+///-----------------------Game Play Function \/ \/
 
-    
-console.log("---")
-  setTimeout(function aa(){
-  console.log("done 10sec")
-  if(video2.srcObject==null){
+function GameStart () {  
+  // $("start").hide()
+  $("hint").text("make you opponent laughing")
+
+  $("#turn").text("your turn")
+  let timeleft = 13;
+  let downloadTimer = setInterval(function(){
+    $("#timerN").text(timeleft);
+    if(timeleft<=0){
+        clearInterval(downloadTimer);
+socket.emit("p2Turn",roomP)
+// break;
+    }
+    timeleft -= 1;
+  }, 1000);
+
+  }
   
-  socket.emit("startV",ROOM_ID,userId)
-  }  
-  }, 6000);
-
-})
-})
-
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ///-----------------------Game Play Function ^^^^^
 socket.on('user-disconnected', userId => {
   if (peers[userId]) peers[userId].close()
   window.location.href = "./playerDisc/id";
 })
 
 myPeer.on('open', id => {
-  userID=id
+  userID = id
   socket.emit('join-room', ROOM_ID, id)
 })
 
 function connectToNewUser(userId, stream) {
- 
+
   const call = myPeer.call(userId, stream)
   const video = document.getElementById('video2')
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
-  console.log(userVideoStream)
+    console.log(userVideoStream)
   })
   call.on('close', () => {
     video.remove()
@@ -128,24 +159,13 @@ function connectToNewUser(userId, stream) {
 
   peers[userId] = call
 }
-let count=0;
+let count = 0;
 function addVideoStream(video, stream) {
-//  if($("#video2")){
-//     console.log("ss")
-//     $("#video2").remove()
-//   }
-  // if(count==2){
 
-  // }else{
-    count++
-  // }
   video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
     video.play()
   })
-  // video.setAttribute("id", `video${count}`);
-    // videoGrid.append(video)
 }
 
 
-    // console.log("eee")  
