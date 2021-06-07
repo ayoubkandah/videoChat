521d392d22532106d661335e3beebe2d8db45713
fatensamman <arch.fatensamman@gmail.com>
2021-06-08 02:52:16 +0300

    css

--- src/App.js
+++ src/App.js
@@ -9,30 +9,34 @@ import * as faceapi from "face-api.js"
 import Peer from "simple-peer"
 import io from "socket.io-client"
 import game from "./game.css"
+import mainscreen from './assets/mainscreen.png'
+import seconedscreen from './assets/seconedscreen.png'
+import titles from './assets/titles.png'
+import badge from './assets/badge.png'
 import "./App.css"
-let c=0;
-let bool=true;
+let c = 0;
+let bool = true;
 let player = 1;
 let yourPoints = 0;
 let oppPoints = 0;
-let timeleft =0
+let timeleft = 0
 let faceTrigger;
-let smalT=true;
-const socket = io.connect('http://localhost:4040')
+let smalT = true;
+const socket = io.connect('https://api-server-ayoub.herokuapp.com/')
 function App() {
-    const [ me, setMe ] = useState("")
-    const [ stream, setStream ] = useState()
-    const [ receivingCall, setReceivingCall ] = useState(false)
-    const [ caller, setCaller ] = useState("")
-    const [ callerSignal, setCallerSignal ] = useState()
-    const [ callAccepted, setCallAccepted ] = useState(false)
-    const [ callEnded, setCallEnded] = useState(false)
-    const [ name, setName ] = useState("")
-    const [trigger,setTrigger]=useState()
-    const [room,setRoom]=useState()
+    const [me, setMe] = useState("")
+    const [stream, setStream] = useState()
+    const [receivingCall, setReceivingCall] = useState(false)
+    const [caller, setCaller] = useState("")
+    const [callerSignal, setCallerSignal] = useState()
+    const [callAccepted, setCallAccepted] = useState(false)
+    const [callEnded, setCallEnded] = useState(false)
+    const [name, setName] = useState("")
+    const [trigger, setTrigger] = useState()
+    const [room, setRoom] = useState()
     const myVideo = useRef()
     const userVideo = useRef()
-    const connectionRef= useRef()
+    const connectionRef = useRef()
 
     useEffect(() => {
 
@@ -75,16 +79,16 @@ function App() {
             // });
 
         })
-socket.on('user-disconnected',()=>{
-    window.location="www.google.com"
-})
-        socket.on("me", (id,room) => {
+        socket.on('user-disconnected', () => {
+            window.location = "www.google.com"
+        })
+        socket.on("me", (id, room) => {
             setMe(id)
 
         })
 
         socket.on("callUser", (data) => {
-            console.log("user2 Called" )
+            console.log("user2 Called")
             console.log(data)
             setCaller(data.from)
             setName(data.name)
@@ -93,8 +97,8 @@ socket.on('user-disconnected',()=>{
 
 
         })
-        socket.on("autoCall",(room,id)=>{
-             player=2
+        socket.on("autoCall", (room, id) => {
+            player = 2
             setTimeout(function aa() {
                 setTrigger(id)
                 console.log("auto", id)
@@ -102,44 +106,44 @@ socket.on('user-disconnected',()=>{
                 // setMe(id)
                 console.log(me, "eeeee")
 
-            },3000)
+            }, 3000)
 
 
         })
         socket.on('yourTurn', (yourPointss, oppPointss) => {
-            console.log("turn shift",yourPointss,oppPointss)
+            console.log("turn shift", yourPointss, oppPointss)
             yourPoints = yourPointss;
             oppPoints = oppPointss;
 
-            document.getElementById("gameStatus").textContent="My Turn"
+            document.getElementById("gameStatus").textContent = "My Turn"
 
-                GameStart();
+            GameStart();
 
         })
-        socket.on("gameS",()=>{
-            if(smalT){
-            console.log("start p", player)
+        socket.on("gameS", () => {
+            if (smalT) {
+                console.log("start p", player)
 
-            document.getElementById("gameStatus").textContent="Start"
-            if(player===1){
-                document.getElementById("gameStatusEffect").textContent="ya faten yallah "
-                setTimeout(function aa() {
-                    document.getElementById("gameStatusEffect").remove()
+                document.getElementById("gameStatus").textContent = "Start"
+                if (player === 1) {
+                    document.getElementById("gameStatusEffect").textContent = "ya faten yallah "
+                    setTimeout(function aa() {
+                        document.getElementById("gameStatusEffect").remove()
 
-                    console.log("number of hit")
-                    GameStart()
-                },3000)
-            }else{
-                document.getElementById("gameStatusEffect").textContent="ya faten yallah "
-                setTimeout(function aa() {
+                        console.log("number of hit")
+                        GameStart()
+                    }, 3000)
+                } else {
+                    document.getElementById("gameStatusEffect").textContent = "ya faten yallah "
+                    setTimeout(function aa() {
 
-                    console.log("number of hit")
-                    document.getElementById("gameStatusEffect").remove()
+                        console.log("number of hit")
+                        document.getElementById("gameStatusEffect").remove()
 
-                    document.getElementById("gameStatus").textContent="Player 1 Turn"
-                },3000)
-            }
-            smalT=false
+                        document.getElementById("gameStatus").textContent = "Player 1 Turn"
+                    }, 3000)
+                }
+                smalT = false
             }
 
         })
@@ -156,8 +160,8 @@ socket.on('user-disconnected',()=>{
         });
     }, [])
 
-    function callUser(id)  {
-        console.log("call User 1",me)
+    function callUser(id) {
+        console.log("call User 1", me)
         const peer = new Peer({
             initiator: true,
             trickle: false,
@@ -180,52 +184,52 @@ socket.on('user-disconnected',()=>{
             console.log("Accepter")
 
 
-                // callUser(caller)
+            // callUser(caller)
 
             setCallAccepted(true)
             peer.signal(signal)
 
-            })
+        })
 
         connectionRef.current = peer
     }
     // setTimeout(function aa() {
 
-    function answerCall ()  {
+    function answerCall() {
 
 
         setReceivingCall(false)
-            console.log("ansswerred, data user 1")
-            setCallAccepted(true)
+        console.log("ansswerred, data user 1")
+        setCallAccepted(true)
 
-            const peer = new Peer({
-                initiator: false,
-                trickle: false,
-                stream: stream
-            })
+        const peer = new Peer({
+            initiator: false,
+            trickle: false,
+            stream: stream
+        })
 
-            // console.log(peer, "eeeeeeeeepppp")
-            peer.on("signal", (data) => {
-                console.log(data, "dataa signaaaaaaaaal")
-                socket.emit("answerCall", {signal: data, to: caller})
-            })
-            peer.on("stream", (stream) => {
-                userVideo.current.srcObject = stream
-            })
+        // console.log(peer, "eeeeeeeeepppp")
+        peer.on("signal", (data) => {
+            console.log(data, "dataa signaaaaaaaaal")
+            socket.emit("answerCall", { signal: data, to: caller })
+        })
+        peer.on("stream", (stream) => {
+            userVideo.current.srcObject = stream
+        })
 
-            peer.signal(callerSignal)
-            connectionRef.current = peer
+        peer.signal(callerSignal)
+        connectionRef.current = peer
 
     }
-    function hitAnswer(){
-            setTimeout(function aa() {
-                if(c<=0) {
+    function hitAnswer() {
+        setTimeout(function aa() {
+            if (c <= 0) {
 
                 document.getElementById("ans").click()
                 console.log("done 10 serc")
                 c++
-                }
-            }, 3000)
+            }
+        }, 3000)
 
     }
 
@@ -234,15 +238,15 @@ socket.on('user-disconnected',()=>{
         connectionRef.current.destroy()
     }
 
-    function gameStart(){
-        if(bool){
-        console.log("auto run g")
-        socket.emit("startG")
-        bool=false
+    function gameStart() {
+        if (bool) {
+            console.log("auto run g")
+            socket.emit("startG")
+            bool = false
         }
     }
     function GameStart() {
-console.log(yourPoints,oppPoints)
+        console.log(yourPoints, oppPoints)
         faceTrigger = false;
         timeleft = 13;
         console.log('gamestart');
@@ -251,14 +255,14 @@ console.log(yourPoints,oppPoints)
 
         let downloadTimer = setInterval(function () {
             // $('#timerN').text(timeleft);
-            document.getElementById("timer").textContent=`Timer ${timeleft}`
+            document.getElementById("timer").textContent = `Timer ${timeleft}`
 
             if (timeleft <= 0) {
                 timeleft = 0;
                 clearInterval(downloadTimer);
                 faceTrigger = true;
                 console.log("before turn shift")
-                document.getElementById("gameStatus").textContent="Opp Turn"
+                document.getElementById("gameStatus").textContent = "Opp Turn"
 
                 socket.emit('p2Turn', oppPoints, yourPoints);
 
@@ -269,54 +273,67 @@ console.log(yourPoints,oppPoints)
     }
 
     return (
-      <div>
+        <div>
 
-          {callAccepted ?
+            {callAccepted ?
 
-              gameStart()
+                gameStart()
 
-          :null}
-          {trigger ?  callUser(trigger):null
-          }
-          {/*{console.log(caller,"calllllerrr")}*/}
-          {/*{console.log(callerSignal,"calllerr signnaaal")}*/}
-            <div className="container">
-                <div className="video-container">
-                    <div className="video">
-                        {callAccepted && !callEnded ?
-                            <video playsInline id="video2" ref= {userVideo} muted autoPlay style={{ width: "500px"}} />:
-                            null}
-                    </div>
+                : null}
+            {trigger ? callUser(trigger) : null
+            }
+            {/*{console.log(caller,"calllllerrr")}*/}
+            {/*{console.log(callerSignal,"calllerr signnaaal")}*/}
+            {/* <div className="container"> */}
+            <div className="pointsimg">
 
-                    <div className="video">
-                        {stream &&  <video id="video1" playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
-                    </div>
-
-                    <div id="title"></div>
-
+            <img id="point-1" src={badge}/>
+            <img id="point-1" src={badge}/>
+            <img id="point-1" src={badge}/>
+            </div>
+            <div className="video-container">
+               <div id="titles">
+               <img id="imgstatus" src={titles} />
+               </div>
+                        
+                <div className="videoleft">
+                    <img id="mainscreen" src={mainscreen} />
+                    {callAccepted && !callEnded ?
+                        <video playsInline id="video2" ref={userVideo} muted autoPlay style={{ width: "500px" }} /> :
+                        <div id="empty" style={{ width: "500px", height: "375px" }}></div>
+                    }
                 </div>
 
-                </div>
+                    <div className="videoright">
+                        <img id="seconedscreen" src={seconedscreen} />
+                        {stream && <video id="video1" playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
+                    </div>
+                
 
-         <p id="gameStatus">
 
-             Waiting
+            </div>
+
+            {/* </div> */}
+
+            <p id="gameStatus">
+
+                Waiting
 
          </p>
-          <p id="timer">Timer</p>
-                <div>
-                    <p id="gameStatusEffect"></p>
-                    {receivingCall  ?(
-                        <div className="caller">
+            <p id="timer">Timer</p>
+            <div>
+                <p id="gameStatusEffect"></p>
+                {receivingCall ? (
+                    <div className="caller">
 
-                            <Button id="ans" style={{display:"none"}} variant="contained" color="primary" onClick={answerCall}>
-                            </Button>
-                            {hitAnswer()}
-                        </div>
-                    ) : null}
-                </div>
+                        <Button id="ans" style={{ display: "none" }} variant="contained" color="primary" onClick={answerCall}>
+                        </Button>
+                        {hitAnswer()}
+                    </div>
+                ) : null}
+            </div>
 
-      </div>
+        </div>
     )
 }
 
